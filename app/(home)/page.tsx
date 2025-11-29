3f69f6e3b5aea294f87cd005a5d5800c032b2792
taek-99 <gunha1215@gmail.com>
2025-11-29 09:39:33 +0900

    nextjs 프로젝트 생성

---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            밥메추 시작
+        </div>
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+
+export default function RootLayout({children} : {children: ReactNode}){
+    return (
+        <html lang='ko'>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
